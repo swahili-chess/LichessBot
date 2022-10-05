e808f269047d48ebe57ff160f12c434977d2e639
Hopertz <lugano.ngulwa@gmail.com>
2022-10-05 22:47:42 +0300

    Enable unsubcribing

--- main.js
+++ main.js
@@ -2,18 +2,38 @@ import { Telegraf } from "telegraf";
 import * as dotenv from "dotenv";
 import { fetchUserSendLink } from "./utils/fetchUserSendLink.js";
 import { writeFileJson } from "./utils/writeFileJson.js";
-import {users} from "./utils/createDictUsers.js"
+import { users } from "./utils/createDictUsers.js"
+import {removeFileJson} from "./utils/removeFileJson"
 dotenv.config();
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.start((ctx, err) => {
+bot.start(async(ctx) => {
   let message = `Please use this bot to get status of Nyumbani mates team members that are online on Lichess`;
-  if (err) {
-    console.log(err);
-  }
-  ctx.reply(message);
-  writeFileJson(ctx.chat.id)
+  try {
+    ctx.reply(message);
+    writeFileJson(ctx.chat.id);
+ } catch (error) {
+       console.log("error", error);
+       
+ }
+  
+  
 });
+
+
+
+bot.command('/stop', async (ctx) => {
+    try {
+       
+      removeFileJson(ctx.chat.id)
+       
+    } catch (error) {
+        console.log('error', error)
+        ctx.reply('Error unsubscribing you to notifications ')
+    }
+})
+
+bot.launch()
 
 fetchUserSendLink(users);
 
---
+++ utils/removeFileJson.js
@@ -0,0 +1,32 @@
+import path from "path";
+import * as fsp from "fs/promises";
+
+const __dirname = path.resolve();
+const file = "userId.json";
+export const pathToJSon = path.join(__dirname, file);
+
+function removeItemOnce(arr, value) {
+  var index = arr.indexOf(value);
+  if (index > -1) {
+    arr.splice(index, 1);
+  }
+  return arr;
+}
+
+
+export async function removeFileJson(id) {
+ 
+    try {
+       const res = await fsp.readFile(pathToJSon, "utf8");
+       const idsObj = JSON.parse(res);
+       if (!idsObj["userId"].includes(id)) {
+          const newArray = removeItemOnce(idsObj["userId"], id);
+          idsObj["userId"]=newArray
+          await fsp.writeFile(pathToJSon, JSON.stringify(idsObj));
+       }
+     } catch (e) {
+        console.log("Error in deleting userId file", e);
+     }
+
+    
+}
